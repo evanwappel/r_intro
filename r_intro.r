a696e0ab2c857aaecb85da6f21ecd29dbe09303b
ewappel@gmail.com <ewappel@gmail.com>
2016-08-22 21:35:51 -0700

    lesson 6

--- r_intro.r
+++ r_intro.r
@@ -493,3 +493,54 @@ order(a)
 positions <- order(planets_df$diameter)
 # Use positions to sort planets_df
 planets_df[positions,]
+
+
+
+
+"Lesson 6: Lists"
+
+"Creating a list"
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+# Construct list with these different elements:
+my_list <- list(my_vector, my_matrix, my_df)
+my_list
+
+"Creating a named list"
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+# Adapt list() call to give the components names
+my_list <- list(my_vector, my_matrix, my_df)
+names(my_list) <- c("vec", "mat", "df")
+# Print out my_list
+my_list
+
+"Creating a named list (2)"
+# The variables mov, act and rev are available
+mov
+act
+rev
+# Finish the code to build shining_list
+shining_list <- list(moviename = mov, actors=act, reviews=rev)
+
+"Selecting elements from a list"
+# shining_list is already pre-loaded in the workspace
+# Print out the vector representing the actors
+shining_list$actors
+# Print the second element of the vector representing the actors
+shining_list$actors[2]
+
+"Adding more movie information to the list"
+# shining_list, the list containing movie name, actors and reviews, is pre-loaded in the workspace
+# We forgot something; add the year to shining_list
+shining_list_full <- c(shining_list , year=1980)
+# Have a look at shining_list_full
+str(shining_list_full)
